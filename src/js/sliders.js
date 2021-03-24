fe7c56aa84c17925513a2e1e7a415456bc20e6d5
Darya Zhihareva <zhihareva.d@mail.ru>
2021-03-24 16:03:26 +0700

    Made reviews-slider

--- src/js/sliders.js
+++ src/js/sliders.js
@@ -1,6 +1,38 @@
 const sliders = () => {
   const Swiper = window.Swiper;
 
+  // Reviews sertificates
+  const reviews = document.querySelector(".js-reviews-slider");
+
+  if (reviews) {
+    const mySwiper = new Swiper(".js-reviews-slider.swiper-container", {
+      direction: "horizontal",
+      slidesPerView: 1,
+      spaceBetween: 20,
+      speed: 400,
+      loop: true,
+      centeredSlides: true,
+      breakpoints: {
+        500: {
+          slidesPerView: 2,
+          spaceBetween: 15,
+        },
+        575: {
+          slidesPerView: 3,
+          spaceBetween: 15,
+        },
+        991: {
+          slidesPerView: 3,
+          spaceBetween: 120,
+        },
+      },
+      navigation: {
+        nextEl: ".js-reviews-slider .swiper-button-next",
+        prevEl: ".js-reviews-slider .swiper-button-prev",
+      },
+    });
+  }
+
   // Slider sertificates
   const sertificates = document.querySelector(".js-sertificates-slider");
 
